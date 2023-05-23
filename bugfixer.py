0e5b032a81e1a8d307b1b8c300ec8f32837a0cd5
hershaw <hershaw@users.noreply.github.com>
2023-05-23 09:23:31 +0000

    [create-pull-request] automated change

--- bugfixer.py
+++ bugfixer.py
@@ -22,14 +22,33 @@ without raising an error.
 MODEL_NAME = 'gpt-3.5-turbo'
 
 
-def _call_llm(llm: ChatOpenAI, message: str):
+def _call_llm(llm: ChatOpenAI, message: str) -> str:
+    """Predicts the response of the language model given a message.
+
+    Args:
+        llm (ChatOpenAI): The language model to use.
+        message (str): The message to send to the language model.
+
+    Returns:
+        str: The response from the language model.
+    """
     return llm.predict_messages([
         SystemMessage(content=SYSTEM_MESSAGE),
         HumanMessage(content=message),
-    ])
+    ]).content
 
 
-def get_file_contents(llm: ChatOpenAI, issue_md: str, prefix: str):
+def get_file_contents(llm: ChatOpenAI, issue_md: str, prefix: str) -> dict:
+    """Returns the contents of all files mentioned in the issue_md.
+
+    Args:
+        llm (ChatOpenAI): The language model to use.
+        issue_md (str): The markdown string containing the issue description.
+        prefix (str): The prefix to add to the file paths.
+
+    Returns:
+        dict: A dictionary containing the contents of each file.
+    """
     prompt = f"""
     {issue_md}
     ==================
@@ -39,7 +58,7 @@ def get_file_contents(llm: ChatOpenAI, issue_md: str, prefix: str):
 
     ["<insert file path 1>", "<insert file path 2>", "..."]
     """
-    file_list = json.loads(_call_llm(llm, prompt).content)
+    file_list = json.loads(_call_llm(llm, prompt))
     contents = {}
     for filename in file_list:
         with open(f'{prefix}/{filename}') as f:
@@ -47,13 +66,30 @@ def get_file_contents(llm: ChatOpenAI, issue_md: str, prefix: str):
     return contents
 
 
-def strip_preamble(llm_output):
+def strip_preamble(llm_output: str) -> str:
+    """Strips the preamble from the language model output.
+
+    Args:
+        llm_output (str): The output from the language model.
+
+    Returns:
+        str: The stripped output.
+    """
     index = llm_output.find('{')
     return llm_output[index:]
 
 
-def fix_bugs(llm: ChatOpenAI, issue_md: str, file_contents: dict):
+def fix_bugs(llm: ChatOpenAI, issue_md: str, file_contents: dict) -> dict:
+    """Fixes the bugs in the file contents using the provided hints.
 
+    Args:
+        llm (ChatOpenAI): The language model to use.
+        issue_md (str): The markdown string containing the issue description.
+        file_contents (dict): A dictionary containing the contents of each file.
+
+    Returns:
+        dict: A dictionary containing the fixed contents of each file.
+    """
     file_contents_json = json.dumps(file_contents)
 
     prompt = f"""
@@ -74,17 +110,17 @@ each file. Then return a JSON dictionary with the same format, where each files
 contents has been corrected. Ensure that the result is safe for JSON
 parsing/dumping.
 """
-    print(prompt)
-    output = strip_preamble(_call_llm(llm, prompt).content)
-
-    print('Response ===============================')
-    print(output)
-    print('End response ===============================')
+    output = strip_preamble(_call_llm(llm, prompt))
 
     return json.loads(output)
 
 
-def main(issue_md):
+def main(issue_md: str) -> None:
+    """Runs the main program.
+
+    Args:
+        issue_md (str): The markdown string containing the issue description.
+    """
     llm = ChatOpenAI(model_name=MODEL_NAME, temperature=0)
     file_contents = get_file_contents(llm, issue_md, '.')
     fixed_bugs = fix_bugs(llm, issue_md, file_contents)
